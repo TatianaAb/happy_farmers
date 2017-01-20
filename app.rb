56ecdb0aa2485ca17bac00b04965ba5705b546c5
Kendy Ng <email": null>
2017-01-20 12:59:51 -0500

    views

--- app.rb
+++ app.rb
@@ -15,13 +15,65 @@ PARSED_DATA = JSON.parse(response)  # gives you array of hashes
 # List of options
 
 # test case when search by borough == Manhattan
-PARSED_DATA.select { |each_hash| each_hash["facilitycity"] == "Manhattan" }
+man = PARSED_DATA.select { |each_hash| each_hash["facilitycity"] == "Manhattan" }
 
 # Get User Input
 
 
-#
+# display model
+
+def print_result(array_of_hashes)
+
+  print_title
+
+  print_data(array_of_hashes)
+
+end
+
+def print_title
+  divider = 205
+  space = 40
+
+  result = ""
+  titles = ["Market Name", "Address", "City", "State", "Zip Code"]
+
+  titles.each do |title|
+      result += "|"
+      result += "#{title}".center(space)
+  end
+  result += "|"
+
+  puts "*" * divider
+  puts result
+  puts "*" * divider
+end
+
+def print_data(array_of_hashes)
+  beg_in = 0
+  end_in = 20
+  space = 40
+  divider = 205
+
+  keys = ["facilityname", "facilitystreetname", "facilitycity", "facilitystate", "facilityzipcode"]
+
+  array_of_hashes.each do |each_hash|
+    temp = ""
+    temp += "|"
+    keys.each do |key|
+      temp += each_hash[key][beg_in, end_in].center(space)
+    temp += "|"
+    end
+    puts temp
+  end
+
+  puts
+  puts "*" * divider
+end
 
 
-[ {   },
-]
+
+print_result(man)
+binding.pry
+
+puts
+
